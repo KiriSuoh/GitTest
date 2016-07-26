78e808711fe96fa1903fdb79f05868be92d51a96
Cyber <cyberchen@outlook.com>
2016-07-26 15:26:47 +0900

    the first commit

---
+++ gitOffline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitOffline
+//
+//  Created by CyberChen on 16/7/26.
+//  Copyright © 2016年 CyberChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
